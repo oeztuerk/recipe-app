ac82a57907089496f0ec80094d09a63f4304be28
Sefa Öztürk <sefa.oeztuerk@porscheinformatik.com>
2021-10-07 16:18:39 +0200

    Refactor code and apply some well practices

--- src/main/java/guru/springframework/recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/recipeapp/domain/Ingredient.java
@@ -25,9 +25,11 @@ public class Ingredient
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
-    public Ingredient()
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom)
     {
-
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe)
